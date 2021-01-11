5362d3c7070002a6b5e6a1cf2b207a2fafa35296
fpistore2020 <fpistore2020@DEV-1-EEL-454.ad.campus-eni.fr>
2021-01-11 18:18:00 +0100

    servlet index

---
+++ src/fr/eni/ProjetENI/servlet/SevletIndex.java
@@ -0,0 +1,27 @@
+package fr.eni.ProjetENI.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SevletIndex")
+public class SevletIndex extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Index.jsp");
+	rd.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
